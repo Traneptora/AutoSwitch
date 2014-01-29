7063f7c310ecfb61a16d5f4bb41bc48ff457f884
Leo Izen <leo.izen@gmail.com>
2014-01-29 11:10:33 -0500

    Added functions and docs

--- src/main/java/thebombzen/mods/autoswitch/NotSoRandom.java
+++ src/main/java/thebombzen/mods/autoswitch/NotSoRandom.java
@@ -2,14 +2,36 @@ package thebombzen.mods.autoswitch;
 
 import java.util.Random;
 
+/**
+ * NotSoRandom provides an implementation of Random that can be used to
+ * obtain determinate results when calculating random occurrences.
+ * E.g. insert it into the World object, call a random function, then replace. 
+ * @author thebombzen
+ */
 public class NotSoRandom extends Random {
 
+	private static final long serialVersionUID = 7668644027932430864L;
+	
 	private boolean useZero;
 
 	public NotSoRandom(boolean useZero) {
 		this.useZero = useZero;
 	}
+	
+	@Override
+	public double nextDouble() {
+		if (useZero){
+			return 0.0D;
+		} else {
+			return 1.0D;
+		}
+	}
 
+	@Override
+	public double nextGaussian(){
+		return nextDouble();
+	}
+	
 	@Override
 	public float nextFloat() {
 		if (useZero) {
