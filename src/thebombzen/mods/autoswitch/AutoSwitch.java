69a5a4cd7444f7ede5395d6d3b25a98e1492ab09
Leo Izen <leo.izen@gmail.com>
2014-02-17 16:31:58 -0500

    Bumped version to 4.3.3

--- src/thebombzen/mods/autoswitch/AutoSwitch.java
+++ src/thebombzen/mods/autoswitch/AutoSwitch.java
@@ -38,7 +38,7 @@ import cpw.mods.fml.relauncher.SideOnly;
  * @author thebombzen
  */
 @SideOnly(Side.CLIENT)
-@Mod(modid = "autoswitch", name = "AutoSwitch", version = "4.3.2", dependencies = "required-after:thebombzenapi", guiFactory = "thebombzen.mods.autoswitch.ConfigGuiFactory")
+@Mod(modid = "autoswitch", name = "AutoSwitch", version = "4.3.3", dependencies = "required-after:thebombzenapi", guiFactory = "thebombzen.mods.autoswitch.ConfigGuiFactory")
 public class AutoSwitch extends ThebombzenAPIBaseMod {
 
 	public static final int STAGE_H0 = 0;
@@ -130,7 +130,7 @@ public class AutoSwitch extends ThebombzenAPIBaseMod {
 
 	@Override
 	public String getLongVersionString() {
-		return "AutoSwitch, version 4.3.2, Minecraft 1.7.2";
+		return "AutoSwitch, version 4.3.3, Minecraft 1.7.2";
 	}
 
 	@Override
