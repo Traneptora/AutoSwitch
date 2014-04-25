0d3456bba35919b5a1f799b5391896c601b67833
Leo Izen <leo.izen@gmail.com>
2014-04-25 17:32:38 -0400

    bumped version to pre9

--- src/thebombzen/mods/autoswitch/AutoSwitch.java
+++ src/thebombzen/mods/autoswitch/AutoSwitch.java
@@ -42,7 +42,7 @@ import cpw.mods.fml.relauncher.SideOnly;
  * @author thebombzen
  */
 @SideOnly(Side.CLIENT)
-@Mod(modid = "autoswitch", name = "AutoSwitch", version = "4.4.0pre8", dependencies = "required-after:thebombzenapi", guiFactory = "thebombzen.mods.autoswitch.configuration.ConfigGuiFactory")
+@Mod(modid = "autoswitch", name = "AutoSwitch", version = "4.4.0pre9", dependencies = "required-after:thebombzenapi", guiFactory = "thebombzen.mods.autoswitch.configuration.ConfigGuiFactory")
 public class AutoSwitch extends ThebombzenAPIBaseMod {
 
 	public static final int STAGE_H0 = 0;
@@ -175,7 +175,7 @@ public class AutoSwitch extends ThebombzenAPIBaseMod {
 
 	@Override
 	public String getLongVersionString() {
-		return "AutoSwitch, version 4.4.0pre8, Minecraft 1.7.2";
+		return "AutoSwitch, version 4.4.0pre9, Minecraft 1.7.2";
 	}
 
 	@Override
