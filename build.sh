24a58c60cea9cc777e593642d116e68d2d4a8529
Leo Izen <leo.izen@gmail.com>
2015-07-16 16:55:03 -0400

    Added build script

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+VERS=5.2.0
+MC_VERS=1.8
+
+cd $(dirname $0)
+ARCH=AutoSwitch-v$VERS-mc$MC_VERS.jar
+./gradlew build
+cp build/libs/autoswitch-$VERS.jar $ARCH
+mkdir -p META-INF
+
+cat >META-INF/MANIFEST.MF <<EOF
+Manifest-Version: 1.0
+Main-Class: thebombzen.mods.autoswitch.installer.ASInstallerFrame
+EOF
+
+zip -u $ARCH META-INF/MANIFEST.MF
+zip -d $ARCH "thebombzen/mods/thebombzenapi*"
+
