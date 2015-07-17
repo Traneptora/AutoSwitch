91f68ee11e9dca370e0249db4889ee33fb8ac428
Leo Izen <leo.izen@gmail.com>
2015-07-16 20:31:48 -0400

    Added String SingleEntityIdentifiers

--- src/thebombzen/mods/autoswitch/configuration/SingleEntityIdentifier.java
+++ src/thebombzen/mods/autoswitch/configuration/SingleEntityIdentifier.java
@@ -7,34 +7,67 @@ import thebombzen.mods.thebombzenapi.configuration.ConfigFormatException;
 
 public class SingleEntityIdentifier implements BooleanTester<EntityLivingBase> {
 	private int id;
+	private String name;
+	
+	public SingleEntityIdentifier(){
+		this.id = -1;
+		this.name = null;
+	}
+	
 	public SingleEntityIdentifier(int id){
 		this.id = id;
+		this.name = null;
+	}
+	
+	public SingleEntityIdentifier(String name){
+		this.id = -1;
+		this.name = name;
 	}
 	
 	public static SingleEntityIdentifier parseSingleEntityIdentifier(String info) throws ConfigFormatException {
 		if (info.equals("@")){
-			return new SingleEntityIdentifier(-1);
+			return new SingleEntityIdentifier();
 		}
 		int id;
 		try {
 			id = Integer.parseInt(info);
 			return new SingleEntityIdentifier(id);
 		} catch (NumberFormatException nfe){
-			throw new ConfigFormatException("Invalid number: " + info, nfe);
+			
 		}
+		if (info.matches("^\\w+$")){
+			return new SingleEntityIdentifier(info);
+		}
+		throw new ConfigFormatException("Illegal SingleEntityIdentifier: " + info);
 	}
 	
 	@Override
 	public boolean contains(EntityLivingBase c) {
-		return id == -1 || EntityList.getEntityID(c) == id;
+		if (id == -1){
+			if (name == null){
+				return true;
+			} else {
+				return EntityList.getEntityString(c).equals(name);
+			}
+		} else {
+			return EntityList.getEntityID(c) == id;
+		}
 	}
+	
+	@Override
+	public String toString() {
+		return id == -1 ? (name == null ? "@" : name) : Integer.toString(id);
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -46,11 +79,11 @@ public class SingleEntityIdentifier implements BooleanTester<EntityLivingBase> {
 		SingleEntityIdentifier other = (SingleEntityIdentifier) obj;
 		if (id != other.id)
 			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
 		return true;
-	}
-
-	@Override
-	public String toString() {
-		return id == -1 ? "@" : Integer.toString(id);
 	}
 }
