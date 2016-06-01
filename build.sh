4771bbf8a558f462d9f826cafbb38e6d3ef2e438
Leo Izen <leo.izen@gmail.com>
2016-06-01 15:51:33 -0400

    Updated buildscript to be standalone

--- build.sh
+++ build.sh
@@ -3,11 +3,33 @@ set -e
 
 VERS=5.3.0
 MC_VERS=1.9
+MDK=1.9.4-12.17.0.1937
+ARCHIVE=AutoSwitch-v$VERS-mc$MC_VERS.jar
 
-cd $(dirname $0)
-ARCH=AutoSwitch-v$VERS-mc$MC_VERS.jar
+CURRDIR="$PWD"
+
+cd "$(dirname $0)"
+mkdir -p build
+cd build
+
+if [ ! -e gradlew ] ; then
+	wget http://files.minecraftforge.net/maven/net/minecraftforge/forge/$MDK/forge-$MDK-mdk.zip
+	unzip forge-1.9.4-12.17.0.1937-mdk.zip
+	./gradlew setupDecompWorkspace
+	./gradlew eclipse
+	rm forge-$MDK-mdk.zip
+	cd src/main
+	rm -rf java resources
+	ln -s ../../../resources
+	ln -s ../../../src java
+	cd ../..
+	rm build.gradle
+	ln -s ../build.gradle
+fi
+
 ./gradlew build
-cp build/libs/autoswitch-$VERS.jar $ARCH
+
+cp build/libs/autoswitch-$VERS.jar $ARCHIVE
 mkdir -p META-INF
 
 cat >META-INF/MANIFEST.MF <<EOF
@@ -15,6 +37,8 @@ Manifest-Version: 1.0
 Main-Class: thebombzen.mods.autoswitch.installer.ASInstallerFrame
 EOF
 
-zip -u $ARCH META-INF/MANIFEST.MF
-zip -d $ARCH "thebombzen/mods/thebombzenapi*"
+zip -u $ARCHIVE META-INF/MANIFEST.MF
+zip -d $ARCHIVE thebombzen/mods/thebombzenapi\*
+
+cp $ARCHIVE "$CURRDIR"
 
