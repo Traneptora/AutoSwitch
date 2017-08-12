2177a0281f20ae0f0b89895fe828518be4bbdef3
Leo Izen <leo.izen@gmail.com>
2017-08-12 14:49:08 -0400

    Constants: Update version constants

--- src/thebombzen/mods/autoswitch/Constants.java
+++ src/thebombzen/mods/autoswitch/Constants.java
@@ -1,6 +1,6 @@
 package thebombzen.mods.autoswitch;
 
 public interface Constants {
-	public static final String VERSION = "5.3.1";
-	public static final String MC_VERSION = "1.9.4";
+	public static final String VERSION = "5.5.0";
+	public static final String MC_VERSION = "1.11.2";
 }
