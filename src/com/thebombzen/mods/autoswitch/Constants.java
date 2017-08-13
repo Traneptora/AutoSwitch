05dd686ff349bb77b1243e62f6a9ead0b95cb3b4
Leo Izen <leo.izen@gmail.com>
2017-08-13 00:32:52 -0400

    Bump version to 5.5.1

--- src/com/thebombzen/mods/autoswitch/Constants.java
+++ src/com/thebombzen/mods/autoswitch/Constants.java
@@ -1,6 +1,6 @@
 package com.thebombzen.mods.autoswitch;
 
 public interface Constants {
-	public static final String VERSION = "5.5.0";
+	public static final String VERSION = "5.5.1";
 	public static final String MC_VERSION = "1.11.2";
 }
