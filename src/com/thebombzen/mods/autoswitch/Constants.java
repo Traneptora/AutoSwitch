5a47735dd4f70c0417cca892783af9401c2138d8
Leo Izen <leo.izen@gmail.com>
2017-09-16 15:47:03 -0400

    Constants.java: Add javadoc describing the field purposes

--- src/com/thebombzen/mods/autoswitch/Constants.java
+++ src/com/thebombzen/mods/autoswitch/Constants.java
@@ -1,7 +1,19 @@
 package com.thebombzen.mods.autoswitch;
 
 public interface Constants {
+
+	/**
+	 * Represents the current version of AutoSwitch.
+	 */
 	public static final String VERSION = "5.6.1";
+
+	/**
+	 * Represents the versions of Minecraft this was built for.
+	 */
 	public static final String SUPPORTED_MC_VERSIONS = "1.9.4 to 1.12.1";
+
+	/**
+	 * Represents the versions of Minecraft this should be installed in.
+	 */
 	public static final String[] INSTALL_MC_VERSIONS = {"1.12.1", "1.11.2", "1.10.2", "1.9.4"};
 }
